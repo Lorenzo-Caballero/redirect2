c55bd675a4212b7e2291a19b732db9e66427c110
Lorenzo Caballero <zeldiuss@gmail.com>
2025-05-10 12:54:39 -0300

    aventur

---
+++ src/Redirect.ts
@@ -0,0 +1,11 @@
+import { useEffect } from "react";
+
+const Redirect = () => {
+  useEffect(() => {
+    window.location.href = "https://miaventura.online/";
+  }, []);
+
+  return null;
+};
+
+export default Redirect;
